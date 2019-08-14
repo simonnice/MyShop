caecc38aece68b79343580b4f63b720c667f50be
Simon Bååth <baathen_swe@hotmail.com>
2019-08-14 15:05:22 +0200

    Product Grouping adding categories

---
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        ProductCategoryRepository context;
+
+        public ProductCategoryManagerController()
+        {
+            context = new ProductCategoryRepository();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategories = context.Collection().ToList();
+            return View(productCategories);
+        }
+
+        public ActionResult Create()
+        {
+            ProductCategory productCategories = new ProductCategory();
+            return View(productCategories);
+        }
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategories)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategories);
+            }
+            else
+            {
+                context.Insert(productCategories);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            ProductCategory productCategories = context.Find(Id);
+            if (productCategories == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCategories);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            ProductCategory productCategories = context.Find(Id);
+
+            if (productCategories == null)
+            {
+                return HttpNotFound();
+
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(productCategories);
+                }
+
+                productCategories.Category = product.Category;
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            ProductCategory productCategoryToDelete = context.Find(Id);
+            if (productCategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+             
+                return View(productCategoryToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            ProductCategory productCategoryToDelete = context.Find(Id);
+            if (productCategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+
+    }
+}
