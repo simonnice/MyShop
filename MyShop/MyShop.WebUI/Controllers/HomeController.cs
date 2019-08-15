0c7c3f4493a012b0e0a1e127df1369d2f5be87c4
Simon Bååth <baathen_swe@hotmail.com>
2019-08-15 11:14:21 +0200

    Changes

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 
 namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
+            List<Product> products = context.Collection().ToList();
             return View();
         }
 
