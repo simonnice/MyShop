e0c39b11ea79ef4b1de936248ea8f9ae41955216
Simon Bååth <baathen_swe@hotmail.com>
2019-08-16 16:05:05 +0200

    Added testing

---
+++ MyShop/MyShop.WebUI.Tests/Mocks/MockHttpContext.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+
+namespace MyShop.WebUI.Tests.Mocks
+{
+    public class MockHttpContext : HttpContextBase
+    {
+        private MockRequest request;
+        private MockResponse response;
+        private HttpCookieCollection cookies;
+
+        public MockHttpContext()
+        {
+            cookies = new HttpCookieCollection();
+            this.request = new MockRequest(cookies);
+            this.response = new MockResponse(cookies);
+        }
+
+        public override HttpRequestBase Request => request;
+
+        public override HttpResponseBase Response => response;
+    }
+
+   
+
+    public class MockResponse : HttpResponseBase
+    {
+        public MockResponse(HttpCookieCollection cookies)
+        {
+            this.Cookies = cookies;
+        }
+
+        public override HttpCookieCollection Cookies { get; }
+    }
+
+    public class MockRequest : HttpRequestBase
+    {
+        public MockRequest(HttpCookieCollection cookies)
+        {
+            this.Cookies = cookies;
+        }
+
+        public override HttpCookieCollection Cookies { get; }
+    }
+
+
+}
